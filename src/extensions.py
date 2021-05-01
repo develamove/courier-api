50d92b3620c15357700f6c0c828ce6d0202fba50
nephite <43841403+nephite@users.noreply.github.com>
2021-05-01 13:36:46 +0800

    Add custom error handlers for JWT manager

--- src/extensions.py
+++ src/extensions.py
@@ -1,3 +1,4 @@
+# from flask import jsonify
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
@@ -9,3 +10,14 @@ database = SQLAlchemy()
 marshmallow = Marshmallow()
 bcrypt = Bcrypt()
 jwt = JWTManager()
+
+# TODO change the jwt default error messages
+
+# @jwt.expired_token_loader
+# def my_expired_token_callback(jwt_header, jwt_payload):
+#     return jsonify(code="dave", err="I can't let you do that"), 401
+#
+#
+# @jwt.unauthorized_loader
+# def my_expired_token_callback(jwt_header, jwt_payload):
+#     return jsonify(code="dave", err="I can't let you do that"), 401
