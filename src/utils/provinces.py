5e9d922984295748c49baed1a329bb4893d06551
nephite <43841403+nephite@users.noreply.github.com>
2021-05-31 15:26:23 +0800

    Change zambales area

--- src/utils/provinces.py
+++ src/utils/provinces.py
@@ -546,7 +546,7 @@ PROVINCES = [
     "name": "Tawi-Tawi"
   },
   {
-    "area": "mindanao",
+    "area": "luzon",
     "created_timestamp": "2021-04-21T04:59:45",
     "id": 79,
     "is_pickup_available": "F",
